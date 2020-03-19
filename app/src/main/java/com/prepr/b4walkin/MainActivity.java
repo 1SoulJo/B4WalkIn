93c1a851d0c4621ccb25cf687a983bd3cc5cb407
root <root@Hansol.localdomain>
2020-03-19 19:16:54 -0400

    First commit

---
+++ app/src/main/java/com/prepr/b4walkin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prepr.b4walkin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
